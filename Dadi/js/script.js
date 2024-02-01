2fd92e6099306df0d15eb41bafd4e77774e63175
alberto <alberto.taddeucci@gmail.com>
2024-02-01 18:06:42 +0100

    inserimento immagini dadi. ES.completato

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -9,6 +9,7 @@ const buttonPlay = document.querySelector("#button-play");
 
 buttonPlay.addEventListener("click",
 function(){
+    const images = ["https://www.tessiland.com/30555/dado-numero-bianco-1.jpg","https://www.tessiland.com/30557/dado-numero-bianco-2.jpg","https://www.tessiland.com/30559/dado-numero-bianco-3.jpg","https://www.tessiland.com/30561/dado-numero-bianco-4.jpg","https://www.tessiland.com/30563-home_default/dado-numero-bianco-5.jpg","https://www.tessiland.com/30565/dado-numero-bianco-6.jpg"]
     const randomNumUser = Math.floor((Math.random() * 6) + 1);
     const randomNumPc = Math.floor((Math.random() * 6) + 1);
 
@@ -26,10 +27,15 @@ function(){
     
     document.querySelector("#resultpc").innerText = `Risultato PC: ${randomNumPc}`
     
-    
+
 
     document.querySelector("#resultuser").innerText = `Il tuo risultato : ${randomNumUser}`
+    
 
+    document.querySelector("#cambio").src = `${images[randomNumPc-1]}`
+    document.querySelector("#cambio2").src = `${images[randomNumUser-1]}`
+
+    
 
 }
 )
